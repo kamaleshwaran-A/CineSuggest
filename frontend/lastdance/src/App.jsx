e5bf7a7a32e2d14b1f4ee4fb2618b27348540b52
kamaleshwaran-A <kamaleshathappan@gmail.com>
2025-04-22 23:58:41 +0530

    Initial commit

---
+++ frontend/lastdance/src/App.jsx
@@ -0,0 +1,92 @@
+import React , {useState,useEffect}from 'react'
+import Search from './Components/Search'
+import Spinner from './Components/Spinner'
+import MovieCard from './Components/MovieCard';
+const API_BASE_URL='https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS={
+  method: 'GET',
+  headers:{
+    accept:'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+const App = () => {
+
+
+ const [searchTerm, setSearchTerm] = useState('');  
+ const[errorMessage,setErrorMessage] = useState(' ')
+ const[movieList,setMovieList]=useState([]);
+ const[isLoading,setIsLoading]=useState(false)
+ 
+ 
+ const fetchMovies = async (query='') => {
+  setIsLoading(true)
+  setErrorMessage('')
+  try{
+    const endpoint=`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+     const  response =await fetch(endpoint,API_OPTIONS);
+     if(!response.ok){
+      throw new Error('Failed to fetch movies');
+     }
+     const data =await response.json();
+
+     if(data.Response ===  'False'){
+      setErrorMessage(data.Error || `Failed to fetch movies`);
+      setMovieList([]);
+      return;
+    }
+
+    setMovieList(data.results || [])
+    
+  }catch(error){
+    console.log(`Error fetching movies: ${error}`);
+    setErrorMessage(`Error fetching movies please try again later`);
+
+    }finally{
+      setIsLoading(false);
+    }
+  
+ }
+
+useEffect(()=>{
+  fetchMovies(searchTerm);
+
+},[searchTerm])
+  return (
+    <main>
+       <div className="pattern" />
+       <div className='wrapper'>
+        <header>
+          <img src="./hero-img.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+       <Search  searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        </header>
+
+
+        <section className="all-movies">
+  <h2 className="mt-[40px]">All Movies</h2>
+
+  {isLoading ? (
+    <Spinner />
+  ) : errorMessage ? (
+    <p className="text-red-500">{errorMessage}</p>
+  ) : (
+    <ul>
+      {movieList.map((movie) => (
+        <MovieCard movie={movie} key={movie.id} />
+
+      ))}
+    </ul>
+  )}
+</section>
+
+       
+       </div>
+    </main>
+  )
+}
+
+export default App
